60698fe81e7f5562265cf2467a8a554e90ab5a4e
Maa-mmy <dikecynthia22@gmail.com>
2023-07-25 13:43:28 +0100

    backend javascript

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
-  
-    if (trueOrFalse) {
-       task = false;
-       task2 = true;
-    }
-  
-    return [task, task2];
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = false;
+    task2 = true;
   }
-  
+
+  return [task, task2];
+}
