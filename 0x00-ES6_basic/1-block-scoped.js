0a10e623e8f5f9b7dbd7df07248949b33df287f7
Cynthia Dike <Maa-mmy>
2023-07-25 07:52:05 +0100

    backend javascript

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-    var task = false;
-    var task2 = true;
+    let task = false;
+    let task2 = true;
   
     if (trueOrFalse) {
+       task = false;
+       task2 = true;
+    }
+  
     return [task, task2];
   }
-}
+  
