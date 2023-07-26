059b54204c09e567f14ceb289a9e3b050a44365e
Maa-mmy <dikecynthia22@gmail.com>
2023-07-26 15:55:56 +0100

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -9,7 +9,7 @@ function handleProfileSignup() {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch((error) => {
-      console.log(error('Signup system offline'));
+      console.log('Signup system offline');
       return new Error('Signup system offline', error);
     });
 }
