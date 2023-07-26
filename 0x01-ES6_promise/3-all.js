1621af43f1861f765b83d5350e1d8e655c5915ed
Maa-mmy <dikecynthia22@gmail.com>
2023-07-26 15:18:22 +0100

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,18 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  const utilsPhoto = uploadPhoto('photo-profile-1');
-  const utilsUser = createUser('Guillaume', 'Salva');
+  const utilsPhoto = uploadPhoto();
+  const utilsUser = createUser();
 
   return Promise.all([utilsPhoto, utilsUser])
     .then(([photoResponse, userResponse]) => {
-      // Handle the responses here if needed
-      const response = {
-        uploadPhoto: photoResponse.body,
-        createUser: `${userResponse.firstName} ${userResponse.lastName}`,
-      };
-      console(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-      return response;
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
     .catch((error) => {
       console.error('Signup system offline', error);
