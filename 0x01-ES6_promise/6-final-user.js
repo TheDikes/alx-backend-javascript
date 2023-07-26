37c16e49d2f329b9a4e33f15a4e9296fc1dab503
Maa-mmy <dikecynthia22@gmail.com>
2023-07-26 18:16:24 +0100

    final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,7 +3,7 @@ import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .allsettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => res.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : String(result.reason),
