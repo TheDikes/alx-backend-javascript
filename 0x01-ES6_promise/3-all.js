1d334a07ba40acf0ee5a6d4d6a726020d43a1d75
Maa-mmy <dikecynthia22@gmail.com>
2023-07-26 15:34:28 +0100

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,18 +1,37 @@
+// import { uploadPhoto, createUser } from './utils';
+
+// function handleProfileSignup() {
+//   const utilsPhoto = uploadPhoto();
+//   const utilsUser = createUser();
+
+//   return Promise.all([utilsPhoto, utilsUser])
+//     .then(([photo, user]) => {
+//       console.log(`${photo.body}`);
+//       console.log(`${user.firstName}`);
+//       console.log(`${user.lastName}`);
+//     })
+//     .catch((error) => {
+//       console.error('Signup system offline', error);
+//       throw error;
+//     });
+// }
+
+// export default handleProfileSignup;
+
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  const utilsPhoto = uploadPhoto();
-  const utilsUser = createUser();
+  const uploadPhotoPromise = uploadPhoto();
+  const createUserPromise = createUser();
 
-  return Promise.all([utilsPhoto, utilsUser])
-    .then(([photo, user]) => {
-      console.log(`${photo.body}`);
-      console.log(`${user.firstName}`);
-      console.log(`${user.lastName}`);
+  return Promise.all([uploadPhotoPromise, createUserPromise])
+    .then(([photoResponse, userResponse]) => {
+      console.log(`Body: ${photoResponse.body}`);
+      console.log(`First Name: ${userResponse.firstName}`);
+      console.log(`Last Name: ${userResponse.lastName}`);
     })
-    .catch((error) => {
-      console.error('Signup system offline', error);
-      throw error;
+    .catch(() => {
+      console.error('Signup system offline');
     });
 }
 
