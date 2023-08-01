e7826c231883f05d25f20a1f2366690471f5aadc
Maa-mmy <dikecynthia22@gmail.com>
2023-08-01 15:36:59 +0100

    Filter

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,5 @@
+export default function getStudentsByLocation(studentsList, city) {
+  const studentsInCity = studentsList.filter((student) => student.location === city);
+
+  return studentsInCity;
+}
