32da80f08182099774429884264cf40cbd524da0
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-09-23 11:57:21 +0100

    3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,15 +1,16 @@
 const sinon = require('sinon');
-const Utils = require('./utils');
 const { expect } = require('chai');
+
+const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const bigBrother = sinon.spy(Utils);
+  it('validates the usage of the Utils function', () => {
+    const bigBrother = sinon.spy(Utils, 'calculateNumber');
 
     sendPaymentRequestToApi(100, 20);
-    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
-    bigBrother.calculateNumber.restore();
+    expect(bigBrother.calledWith('SUM', 100, 20)).to.be.true;
+    expect(bigBrother.callCount).to.be.equal(1);
+    bigBrother.restore();
   });
 });
