500ef528feca3a3994b1e8209b30979f5472ab53
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-09-23 17:22:50 +0100

    4-payment.test.js

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -5,14 +5,14 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
+  it('validate the usage of the Utils function', () => {
     const spyConsole = sinon.spy(console, 'log');
     const stubUtils = sinon.stub(Utils, 'calculateNumber');
 
    stubUtils.withArgs('SUM', 100, 20).returns(10);
     sendPaymentRequestToApi(100, 20);
-    chai.expect(spyConsole.calledOnce).to.be.true;
-    chai.expect(spyConsole.calledWith('The total is: 10')).to.be.true;
+    expect(spyConsole.calledOnce).to.be.true;
+    expect(spyConsole.calledWith('The total is: 10')).to.be.true;
     stubUtils.restore()
     spyConsole.restore();
   });
